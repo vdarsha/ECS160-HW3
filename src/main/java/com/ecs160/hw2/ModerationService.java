798c6bfee9c86bdc2502b3ad7c44bb768772f3b7
T Palit <tpalit@cs.stonybrook.edu>
2024-12-16 15:34:11 -0800

    Initial commit

---
+++ src/main/java/com/ecs160/hw2/ModerationService.java
@@ -0,0 +1,13 @@
+package com.ecs160.hw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModerationService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModerationService.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecs160/hw2/ModerationServiceTests.java
@@ -0,0 +1,13 @@
+package com.ecs160.hw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModerationServiceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
